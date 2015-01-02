de05c3cb50c60123493d0d2e634d69630ab6d515
Christopher L Bray <chris@brayniverse.com>
2015-01-02 23:43:57 +0000

    Initial commit - just setting the tone

---
+++ Container/Container.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * S H U T T L E - a minimalist PHP framework
+ *
+ * @package Shuttle\Contracts
+ * @author  Christopher L Bray <chris@brayniverse.com>
+ * @license MIT
+ */
+
+namespace Shuttle\Contracts\Container;
+
+use Closure;
+
+/**
+ * Describes the minimum expectations for a dependency container for extending
+ * the Shuttle framework.
+ */
+interface ContainerInterface
+{
+	
+}
