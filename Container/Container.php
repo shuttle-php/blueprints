9104a077350e67866a79466faa8171f2747887d7
Christopher L Bray <chris@brayniverse.com>
2015-03-31 17:14:00 +0100

    Add minimum functionality to interface (see #1)
    This adds three new methods to the interface `register`, `resolve`, and `exists` which makes it possible add and retrieve items from the container.

--- Container/Container.php
+++ Container/Container.php
@@ -10,13 +10,38 @@
 
 namespace Shuttle\Contracts\Container;
 
-use Closure;
-
 /**
- * Describes the minimum expectations for a dependency container for extending
- * the Shuttle framework.
+ * A service container is a collection of services which can be resolved at
+ * any time. This interface defines the basic methods required to interact
+ * with a service container.
  */
 interface Container
 {
-	
+    /**
+     * Insert a new container item.
+     * 
+     * @param string $alias
+     * @param ContainerItem $value
+     * @return self
+     */
+    public function register($alias, ContainerItem $value);
+
+    /**
+     * Resolve a container item.
+     * 
+     * @param string $alias
+     * @return mixed ContainerItem registered under the provided alias.
+     *
+     * @throws \Shuttle\Contracts\Container\ContainerItemMissing
+     *     If the alias does not match any registered services.
+     */
+    public function resolve($alias);
+
+    /**
+     * Determine whether an item has been registered under the specified key.
+     * 
+     * @param string $alias
+     * @return boolean True if the item exists, otherwise false.
+     */
+    public function exists($alias);
 }
